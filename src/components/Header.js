b88de07dd0b82e5e6f09f2c8455b246adee91793
AJ Mendoza <mendozaasaph@gmail.com>
2022-03-29 22:55:01 +0800

    Finished Tutorial

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
-const Header = ({title}) => {
-  const onClick = () => {
-    console.log('Click')
-  }
+const Header = ({title ,onAdd, showAdd}) => {
+  const location = useLocation()
 
   return (
     <header className='header'>
       {/* <h1 style={headingStyle}>{title}</h1> */}
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      { location.pathname === '/' && (
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+      )}
     </header>
   )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,13 @@
-const Task = ({ task }) => {
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className='tasks'>
-      <h3 key={task.id}>{task.text}</h3>
+  <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+      <h3 key={task.id}>
+        {task.text} 
+        <FaTimes style={{ color: 'red', cursor: 'pointer' }} 
+                 onClick={() => onDelete(task.id)} />
+      </h3>
       <p>{task.day}</p>
     </div>
   )
